4d7bca7d3b0ea15d16c4afdc5b4321eaf16183be
RyanHangai <ryanhangai.v@gmail.com>
2026-06-07 22:08:32 -0300

    Adiciona validacao de tamanho

--- src/ultilitario.py
+++ src/ultilitario.py
@@ -1,5 +1,11 @@
 import re
 
+def contar_caracteres(texto):
+    """Conta quantos caracteres tem no parâmetro e retorna o valor como inteiro."""
+    if not texto:
+        return 0
+    return len(texto)
+
 def exclui_espaco(texto):
     """Remove espaços no começo e no final da string."""
     if not texto:
@@ -15,6 +21,8 @@ def limpar_texto(texto):
 def valida_senha(senha):
     """Valida se a senha atende aos requisitos de segurança."""
     erros = []
+    if contar_caracteres(senha) > 255:
+        erros.append('A senha não pode passar de 255 caracteres.')
     if len(senha) < 8:
         erros.append('A senha deve ter no mínimo 8 caracteres.')
     if not any(char.isupper() for char in senha):
@@ -30,7 +38,7 @@ def valida_senha(senha):
 def valida_nome(nome):
     """Valida se o nome do usuário é válido (para cadastro de conta)."""
     erros = []
-    if len(nome) > 255:
+    if contar_caracteres(nome) > 255:
         erros.append('O nome não pode passar de 255 caracteres.')
     espacos = nome.count(' ')
     if espacos < 1:
@@ -44,13 +52,16 @@ def valida_email(email):
     if not email:
         return False
 
+    if contar_caracteres(email) > 255:
+        return False
+
     # Verifica se tem espaços ou vírgulas
     if ' ' in email or ',' in email:
         return False
 
     if email.count('@') != 1:
         return False
-    
+
     partes = email.split('@')
     usuario = partes[0]
     dominio_completo = partes[1]
@@ -58,16 +69,16 @@ def valida_email(email):
     # Garante que haja pelo menos 2 caracteres antes do @
     if len(usuario) < 2:
         return False
-    
+
     # Verifica se tem ponto no domínio e se há algo entre o @ e o primeiro ponto
     if '.' not in dominio_completo:
         return False
-    
+
     # Divide o domínio para verificar a "palavra" entre @ e .
     nome_dominio = dominio_completo.split('.')[0]
     if len(nome_dominio) < 1:
         return False
-        
+
     return True
 
 def main():
